7baf3ad8e9739dc333b90bc3ba5b2d392cf5327d
Joseba Carral <jcarraldc@gmail.com>
2017-02-27 23:59:58 +0100

    Añadir ruta a firebase

--- controllers/route.ctrl.js
+++ controllers/route.ctrl.js
@@ -4,13 +4,30 @@ const Response = require('../lib/responses')
 const Location = require('../lib/location')
 const Parser = require('../lib/parsers')
 const Route = require('../models/route.model')
-
+const db = require('../config/initializers/database').getDB()
 
 exports.add = (req, res) => {
-  let data = req.body.route
+    let data = req.body.route
 
-  if(typeof data === 'undefined') return Response.invalidParams(res)
-  else if(!Route.validRoute(data)) return Response.invalidFormat(res)
-
-  res.send({mssg: 'Route added'})
+    if (typeof data === 'undefined') return Response.invalidParams(res)
+    else if (!Route.validRoute(data)) return Response.invalidFormat(res)
+    let newRoute = db.database().ref('routes/').push()
+    let postData = {}
+    postData[newRoute.key] = data
+    db.database().ref('routes/').update(postData)
+        .then((data) => {
+            //TODO: Normalizar respuestas
+            res.statusCode = 200
+            res.send({
+                message: 'Ruta almacenada correctamente',
+                key: newRoute.key
+            })
+        })
+        .catch(err => {
+          console.log(err);
+            Response.internalError(res, {
+                desc: `Error al internar almacenar en firebase una ruta. \n ${err}`,
+                func: 'routes.add'
+            })
+        })
 }
